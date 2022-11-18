70b63214d15cd74d9a60b9e5ca203b1035167a24
Fyl Andrii <fil.andrey91@gmail.com>
2022-11-18 09:40:33 +0200

    adds functionality and styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,7 @@ const colorPickerOptions = [
   { label: 'green', color: '#17b514' },
   { label: 'blue', color: '#0cf2db' },
   { label: 'yellow', color: '#f2ee0c' },
+  { label: 'violet', color: '#ed1ba4' },
   { label: 'orange', color: '#f29e0c' },
 ];
 
@@ -14,36 +15,3 @@ export const App = () => (
     <ColorPicker options={colorPickerOptions} />
   </div>
 );
-
-// render() {
-//     return (
-//       <div className={css.ColorPicker}>
-//         {this.props.options.map((option, index) => {
-//           // Как дообавить несколько классов на один элемент:
-//           // 1 - в переменную бросаем основной клас
-//           const optionClasses = ['ColorPicker__option'];
-//           //  2-   по условию, если индекс елемента на который мы нажимаем === actOptionIdx,
-//           //  то в массив с основным индексом пушим дополнительный: получакм новый массив [ColorPicker__option, ColorPicker__option__active]
-//           if (index === this.state.actOptionIdx) {
-//             optionClasses.push('ColorPicker__option__active');
-//           }
-//           return (
-//             <button
-//               type="button"
-//               key={option.label}
-//               // 3 - в  className через переменную распыляем содержимое массива, разделив названием классов пробелом
-//               // если их там два, то в className получим: className = ColorPicker__option  ColorPicker__option__active
-//               className={optionClasses.join(' ')}
-//               style={{
-//                 backgroundColor: option.color,
-//                 border:
-//                   index === this.state.actOptionIdx
-//                     ? '2px solid black'
-//                     : 'none',
-//               }}
-//             ></button>
-//           );
-//         })}
-//       </div>
-//     );
-//   }
--- src/components/ColorPicker/ColorPicker.jsx
+++ src/components/ColorPicker/ColorPicker.jsx
@@ -3,33 +3,63 @@ import css from './ColorPicker.module.css';
 
 class ColorPicker extends React.Component {
   state = {
-    actOptionIdx: 2,
+    actOptionIdx: 0,
+  };
+
+  // метод для получения активного активного индекса (при клике на кнопку)
+  setActiveIdx = index => {
+    this.setState({ actOptionIdx: index });
+  };
+
+  // создаем метод класса для добавления дополнительного класса
+  makeOptionClassName = index => {
+    //1 - в переменную бросаем основной клас
+    const optionClasses = [css.ColorPicker__option];
+    //  2-   по условию, если индекс елемента на который мы нажимаем === actOptionIdx,
+    //  то в массив с основным классом пушим дополнительный: получакм
+    // новый массив[ColorPicker__option, ColorPicker__option__active]
+    if (index === this.state.actOptionIdx) {
+      optionClasses.push(css.ColorPicker__option__active);
+    }
+    // возвращаем из метода makeOptionClassName наши классы из массива optionClasses (разделив пробелом)
+    // например css.ColorPicker__option  css.ColorPicker__option__active
+    return optionClasses.join(' ');
   };
 
   render() {
+    // деструктуризируем пропсы, стейт, методы
+    const { options } = this.props;
+    const { actOptionIdx } = this.state;
+    const { makeOptionClassName, setActiveIdx } = this;
+
+    // Вычисляемые свойства делаем в методе render (вычисляются на базе пропсов и стейта)
+    const currentColor = options[actOptionIdx];
+
     return (
       <div className={css.ColorPicker}>
-        {this.props.options.map((option, index) => {
-          const optionClasses = [css.ColorPicker__option];
-          if (index === this.state.actOptionIdx) {
-            optionClasses.push(css.ColorPicker__option__active);
-          }
-
-          return (
-            <button
-              type="button"
-              key={option.label}
-              className={optionClasses.join(' ')}
-              style={{
-                backgroundColor: option.color,
-                border:
-                  index === this.state.actOptionIdx
-                    ? '2px solid black'
-                    : 'none',
-              }}
-            ></button>
-          );
-        })}
+        <p className={css.ColorName}>
+          You chose color:
+          <span
+            style={{
+              color: currentColor.color,
+              marginLeft: '10px',
+              fontWeight: '600',
+            }}
+          >
+            {currentColor.label}
+          </span>
+        </p>
+        {options.map(({ label, color }, index) => (
+          <button
+            type="button"
+            key={label}
+            //  результат вызова метода класса makeOptionClassName запишем в наш класс
+            className={makeOptionClassName(index)}
+            style={{ backgroundColor: color }}
+            // React под капотом вешает слушателя событий на каждый элемент (onClick, onSubmit, onChange)
+            onClick={() => setActiveIdx(index)}
+          ></button>
+        ))}
       </div>
     );
   }
